74ca031dba4bbf2091d52a5d833d3729760d6b20
tech-camp <>
2018-06-26 12:16:08 +0900

    #50 コントローラの作成(雛形)

---
+++ app/Http/Controllers/CurreachsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CurreachsController extends Controller
+{
+  public function index()
+  {
+      return view('curreachs.index');
+  }
+
+  public function show()
+  {
+      return view('curreachs.show');
+  }
+}
