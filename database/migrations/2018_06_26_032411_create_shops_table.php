a5ead3839b531c97c053791eff731cab9b16e996
tech-camp <tech-camp@1-177.local>
2018-06-26 12:33:25 +0900

    Shopモデル、shopsテーブルを追加

---
+++ database/migrations/2018_06_26_032411_create_shops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('shop_name');
+            $table->double('lat', 9, 7);
+            $table->double('lng', 10, 7);
+            $table->string('address')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shops');
+    }
+}
