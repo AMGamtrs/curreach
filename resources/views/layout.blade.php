c91949c4a31491e667d8146467ab645121269711
tech-camp <>
2018-06-28 12:37:52 +0900

    Bootstrapでナビバー追加、loginページを中央揃えにしました

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <title>curreach</title>
         <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/assets/stylesheets/bootstrap.css">
+        <link rel="stylesheet" href="/assets/stylesheets/bootstrap-theme.css">
   </head>
 
   <body>
-    <header class="header">
+<!--    <header class="header">
       <div class="header__bar row">
         <h1 class="grid-6"><a href="/">curreach</a></h1>
         @if (Auth::check())
@@ -30,8 +32,69 @@
           </div>
         @endif
       </div>
-    </header>
-    
+    </header>-->
+
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">crreach</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+
+
+
+      <script src="assets/javascripts/jquery-3.1.1.js"></script>
+      <script src="assets/javascripts/bootstrap.js"></script>
+
+
+
     @yield('content')
   </body>
 </html>
