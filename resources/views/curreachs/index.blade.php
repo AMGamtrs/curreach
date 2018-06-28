6ad5e37e4b6d0edb7133579d99c2d38700ff169e
isshinQ <qiuyixin1992817@yahoo.co.jp>
2018-06-28 09:59:13 +0900

    トップページを編集した

---
+++ resources/views/curreachs/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+  <ul class="sideMenu">  
+    <li><a href="/cat1/">地図から探す</a></li>  
+    <li><a href="/cat2/">詳し検索</a></li>  
+    
+</ul>  
+@endsection
