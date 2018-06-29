2be5b543043843f7ba0351539a5dd5868d74d2fc
tech-camp <>
2018-06-29 09:37:00 +0900

    UserControllerのshowアクション作成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Auth;
+use App\User;
 
 class UsersController extends Controller
 {
-    //
+  public function show($id)
+  {
+      $user = User::find($id);
+      // ユーザのカレーのレビューやお気に入りを送る (今後実装)
+      //$curries = User::find($id)->curries()->orderBy('created_at', 'DESC')->paginate(5);
+      return view('users.show')->with('user', $user);//(array('name' => $name, 'curries' => $curries));
+  }
 }
