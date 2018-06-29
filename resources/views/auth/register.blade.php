a1d8d67578c7854d616f1cf4157784a11bd8ed36
tech-camp <>
2018-06-29 13:15:23 +0900

    Implementation of login function

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,54 +1,89 @@
 @extends('layout')
 
+
 @section('content')
-<div class="contents row">
-    <h2>新規登録</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">新規登録</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                        {{ csrf_field() }}
 
-    {{ Form::open() }}
-        <div class="field">
-            <label>ユーザID(ニックネーム)<label><br>
-            <input type='text' name="name" autofocus="autofocus">
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">ユーザID<br/>(ニックネーム)</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">メールアドレス</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                      <!-- 生年月日入力(余裕があれば実装したい) -->
+                      <!--<div class="field>
+                        <label for="date">生年月日:</label><br>
+                        <input type="date" name="calendar" max="9999-12-31" required>
+                      </div> -->
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">パスワード</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">パスワード確認</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-warning">
+                                    <i class="fa fa-btn fa-user"></i> 登録
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="field">
-            <label>メールアドレス</label><br>
-            <input type="email" name="email">
-        </div>
-          <label for="date">生年月日:</label><br>
-  <input type="date" name="calendar" max="9999-12-31" required><br>
-
-<form>
-  <div>
-    <label for="password_1">パスワード:</label>
-  </div>
-  <div>
-    <input type="password" id="password_1" required />
-  </div>
-  <div>
-    <label for="passwordConfirm_1">メーパスワード確認用:</label>
-  </div>
-  <div>
-    <input type="password" id="passwordConfirm_2" required oninput="CheckPassword_1(this)" />
-  </div>
-  <p>
-    <input type="submit" id="submit">
-  </p>
-</form>
-
-<script language="JavaScript" type="text/javascript">
-<!--
-  function CheckPassword_1(input){
-    var password = password_1.value; //パスワードフォームの値を取得
-    var passwordConfirm = input.value; //パスワード確認用フォームの値を取得(引数input)
-
-    // パスワードの一致確認
-    if(password != passwordConfirm){
-      input.setCustomValidity('パスワードが一致しません'); // エラーメッセージのセット
-    }else{
-      input.setCustomValidity(''); // エラーメッセージのクリア
-    }
-  }
-// -->
-</script>
-
+    </div>
+</div>
 @endsection
