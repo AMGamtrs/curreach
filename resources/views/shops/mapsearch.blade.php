8afa9b4598aedc604df5a9d547bb05d02d9d4ba0
tech-camp <tech-camp@1-177.local>
2018-06-29 12:18:47 +0900

    マーカを独自画像に変更

--- resources/views/shops/mapsearch.blade.php
+++ resources/views/shops/mapsearch.blade.php
@@ -3,52 +3,50 @@
 @section('content')
 
 <div class="row">
-  <div class="col-xs-2">
-    <p>メニューメニューメニュー</p>
-    <ul class="menuuu">
+  <div class="col-xs-2"><!-- 左メニュー -->
+    <p>検索結果</p>
+    <ul class="shop_list">
     </ul>
   </div>
-  <div class="col-xs-10">
-    <p>Hello maps!</p>
-
+  <div class="col-xs-10"><!-- 右地図表示領域 -->
     <!-- 地図を表示する -->
     <div id="map" style="width: 90%; height: 500px"></div>
+
     <script>
-      //var marker;
       var markers = Array();
       var currentInfoWindow;
       function initMap(){
 
-        var tokyo = {lat: 35.6808586, lng: 139.7669568};
+        var map_center = {lat: 35.6808586, lng: 139.7669568};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
-          center: tokyo
+          center: map_center
         });
 
+        //地図移動時にマーカ再set
         google.maps.event.addListener(map, 'idle', function(){
           setPointMarker();
         });
 
+        //マーカを表示する
         function newMark(response){
             var i = markers.length;
-            // console.log(i);
             markers[i] = new google.maps.Marker({
               position: {lat: response['lat'], lng: response['lng']},
               map: map,
-              //icon: "{{ asset('assets/img/small.png') }}"
-              //icon: "http://localhost:8000/assets/favicon.png"
+              icon: "{{ asset('assets/images/marker_small.png') }}"
           });
           //吹き出し表示
           var infoWnd = new google.maps.InfoWindow({
-            content: response['name'] + "<p>あいうえお<p>",
+            content: response['shop_name'] + "<p>その他の情報<p>",
           });
           //マーカクリック時の処理
           google.maps.event.addListener(markers[i], "click", function(){
-                //markers[i].setIcon("{{asset('assets/img/big.png')}}");
+                markers[i].setIcon("{{asset('assets/images/marker_big.png')}}");
                 //先に開いた情報ウィンドウがあれば、closeする iconも戻す
                 if (currentInfoWindow) {
                   currentInfoWindow.close();
-                  //nowM.setIcon( "{{asset('assets/img/small.png')}}" );
+                  nowM.setIcon( "{{asset('assets/images/marker_small.png')}}" );
                 }
                 //情報ウィンドウを開く
                 infoWnd.open(map, markers[i]);
@@ -58,27 +56,28 @@
           });
         }
 
+
         onload=function(){
+          console.log("onload");
           $(function(){
               $('li').click(function(){
                   var index = $(this).index();
-                  console.log('要素:' + index + '番目');
                   google.maps.event.trigger(markers[index], "click");
               });
           });
         }
 
         function setPointMarker(){
+          console.log("setPointMarker");
           //古いマーカを削除
           if(markers.length > 0){
             for (i = 0; i <  markers.length; i++) {
               markers[i].setMap(null);
-              // console.log("delete");
             }
+            //店舗リスト削除
+            $('ul.shop_list').empty();
             //配列削除
-              markers = [];
-            //リスト削除
-            $('ul.menuuu').empty();
+            markers = [];
           }
           //地図の東西南北境界取得
           var bounds = map.getBounds();
@@ -95,16 +94,18 @@
             timeout: 1000,
             data: map_latlng
           }).done(function(responseData) {
-            var Mnum = 0;
+            //店舗リスト削除(ウィンドウサイズ変更時対策ここに入れるとマーカ大きくならない)
+            //$('ul.shop_list').empty();
+            console.log("ajax");
+            var menu_n = 0;
             responseData.forEach(function(response){
               newMark(response);
-              //ここでリスト表示したい
-              ShopList = $('ul.menuuu').append("<li id=" + Mnum + "><a href=\"javascript:void(0)\">" + response['shop_name'] + "</a></li>");
-              Mnum = Mnum + 1;
+              //ここでリスト表示する
+              ShopList = $('ul.shop_list').append("<li id=" + menu_n + "><a href=\"javascript:void(0)\">" + response['shop_name'] + "</a></li>");
+              menu_n = menu_n + 1;
             });
           }).fail(function(error) {
             console.log(error);
-            console.log('nnn');
           });
         }
       }
@@ -116,8 +117,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script
-async defer
-src ="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvoocj2Tn6Kc08gKXjZT5sUYfJ1KPqOwg&callback=initMap">
+  async defer
+  src ="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvoocj2Tn6Kc08gKXjZT5sUYfJ1KPqOwg&callback=initMap">
 </script>
 
 
