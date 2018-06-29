311af2285c62ef8e3c37b0d547a7ffe22237293b
tech-camp <tech-camp@1-177.local>
2018-06-29 18:05:10 +0900

    地図を追加 マーカ移動後の座標を取得

--- resources/views/shops/create.blade.php
+++ resources/views/shops/create.blade.php
@@ -21,10 +21,50 @@
   <input type="text" name="item"><nobr></p>
 </div>
 
+<div id="map" style="width: 90%; height: 300px"></div>
+
 <div class="actions">
     <input type="submit" value="登録">
 </div>
 {{ Form::close() }}
 
 </div>
+
+<script>
+  function initMap(){
+    var map_center = {lat: 35.6808586, lng: 139.7669568 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: map_center
+    });
+
+    //ドラッグ可能なマーカを追加
+    var marker = new google.maps.Marker({
+      draggable:true,
+      position: map_center,
+      map: map,
+      icon: "{{ asset('assets/images/marker_small.png') }}"
+    });
+
+    //マーカークリックで吹き出し表示  住所を表示
+
+    //マーカドラッグ後の座標を取得
+    google.maps.event.addListener( marker, 'dragend', function(){
+      var pos = marker.getPosition();
+      var poslat = pos.lat();
+      var poslng = pos.lng();
+      console.log(poslat);
+    });
+
+  }
+</script>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script
+  async defer
+  src ="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvoocj2Tn6Kc08gKXjZT5sUYfJ1KPqOwg&callback=initMap">
+</script>
+
 @endsection
