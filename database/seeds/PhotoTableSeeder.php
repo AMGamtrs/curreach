2c507a5ca26311b2bd28b1223f08ad4163d7e37c
tech-camp <tech-camp@1-177.local>
2018-07-13 12:28:11 +0900

    photosの空白データをnullに変更

--- database/seeds/PhotoTableSeeder.php
+++ database/seeds/PhotoTableSeeder.php
@@ -14,11 +14,11 @@ class PhotoTableSeeder extends Seeder
       DB::table('photos')->truncate();
       $photos = array(
 
-        array('shop_id' => '', 'curry_id' => '1', 'review_id' => '', 'image' => '1HSxvQ2U--3jTv8Y_bebYZA-P7JOD4l24'),
-        array('shop_id' => '', 'curry_id' => '2', 'review_id' => '', 'image' => '1VDHCCBbr__-7HRpFLz9hyFDesJ4YHbfJ'),
-        array('shop_id' => '', 'curry_id' => '3', 'review_id' => '', 'image' => '1oL52yJ9gkceam-pstegoHTnCO_FulHQO'),
-        array('shop_id' => '1', 'curry_id' => '', 'review_id' => '', 'image' => '1X_cTZGTMj5ZRDJ1qx5bcIv4eDDBq-qJS'),
-        array('shop_id' => '2', 'curry_id' => '', 'review_id' => '', 'image' => '1kfYl_xrJQrM8NOxCmkKlGbFyqXUV9oRr'),
+        array('shop_id' => null, 'curry_id' => '1', 'review_id' => null, 'image' => '1HSxvQ2U--3jTv8Y_bebYZA-P7JOD4l24'),
+        array('shop_id' => null, 'curry_id' => '2', 'review_id' => null, 'image' => '1VDHCCBbr__-7HRpFLz9hyFDesJ4YHbfJ'),
+        array('shop_id' => null, 'curry_id' => '3', 'review_id' => null, 'image' => '1oL52yJ9gkceam-pstegoHTnCO_FulHQO'),
+        array('shop_id' => '1', 'curry_id' => null, 'review_id' => null, 'image' => '1X_cTZGTMj5ZRDJ1qx5bcIv4eDDBq-qJS'),
+        array('shop_id' => '2', 'curry_id' => null, 'review_id' => null, 'image' => '1kfYl_xrJQrM8NOxCmkKlGbFyqXUV9oRr'),
       );
 
       DB::table('photos')->insert($photos);
