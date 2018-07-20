6ca5c4f3fb39504a245c53843eb60cf77567ebc5
tech-camp <>
2018-07-20 11:30:19 +0900

    Fix login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-warning">
+            <div class="panel panel-warning" style="padding:0;">
                 <div class="panel-heading">新規登録</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}" enctype="multipart/form-data">
@@ -97,11 +97,9 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-5 col-sm-offset-5">
-                                <button type="submit" class="btn btn-warning">
+                                <button type="submit" class="btn btn-warning center-block">
                                     <i class="fa fa-btn fa-user"></i> 登録
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </div>
