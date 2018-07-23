0bb49547e81bb710f8c566b09e8aa2ea8f925aad
tech-camp <>
2018-07-23 11:36:10 +0900

    Enable to register review

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Review;
 use App\Photo;
+use DB;
+use File;
+use Storage;
 use Auth;
 use Image;
 
@@ -14,10 +17,6 @@ class ReviewsController extends Controller
 {
   public function curryreview(Request $request)
   {
-      // 写真を保存
-      $fileName = $request->picture->getClientOriginalName();
-      Image::make($request->picture)->save(public_path() . '/images/reviews/' . $fileName);
-
       $reviewdb = new Review();
       //レビューDBに入力
       $reviewdb->review = $request->review;
@@ -26,32 +25,52 @@ class ReviewsController extends Controller
       $reviewdb->user_id = Auth::user()->id;
       $reviewdb->save();
 
-      //写真DBに入力
-      $photo = new Photo();
-      $photo->image = $fileName;
-      $photo->review_id = $reviewdb->id;
-      $photo->save();
-      return redirect('/');
+      // 写真を保存
+      if(!empty($request->picture)){
+        $fileName = $request->picture->getClientOriginalName();
+        //Image::make($request->picture)->save(public_path() . '/images/reviews/' . $fileName);
+        // 写真をドライブに保存
+        $fileData = File::get($request->picture);
+        Storage::disk('reviews_google')->put($fileName, $fileData);
+        //写真DBに入力
+        $photo = new Photo();
+        //$photo->image = $fileName;
+        $drivename = Storage::disk('reviews_google')->url($fileName);
+        $drivename = substr($drivename, 31, -13);
+        $photo->image = $drivename;
+        $photo->review_id = $reviewdb->id;
+        $photo->save();
+      }
+      return redirect("/shops/".DB::table('curries')->where('id', $request->curry_id)->value('shop_id')."/curries/".$request->curry_id);
   }
 
   public function shopreview(Request $request)
   {
-      // 写真を保存
-      $fileName = $request->picture->getClientOriginalName();
-      Image::make($request->picture)->save(public_path() . '/images/reviews/' . $fileName);
-
-      $reviewdb = new Reviews();
+      $reviewdb = new Review();
       //レビューDBに入力
       $reviewdb->review = $request->review;
-      $review->rate = $request->rate;
-      $review->user_id = Auth::user()->id;
-      $review->save();
+      $reviewdb->rate = $request->rate;
+      $reviewdb->shop_id = $request->shop_id;
+      $reviewdb->user_id = Auth::user()->id;
+      $reviewdb->save();
 
-      //写真DBに入力
-      $photo = new Photo();
-      $photo->image = $fileName;
-      $photo->review_id = $reviewdb->id;
-      $photo->save();
-      return redirect('/');
+      // 写真を保存
+      if(!empty($request->picture)){
+        $fileName = $request->picture->getClientOriginalName();
+        //Image::make($request->picture)->save(public_path() . '/images/reviews/' . $fileName);
+        // 写真をドライブに保存
+        $fileData = File::get($request->picture);
+        Storage::disk('reviews_google')->put($fileName, $fileData);
+        //写真DBに入力
+        $photo = new Photo();
+        //$photo->image = $fileName;
+        $drivename = Storage::disk('reviews_google')->url($fileName);
+        $drivename = substr($drivename, 31, -13);
+        $photo->image = $drivename;
+        $photo->review_id = $reviewdb->id;
+        $photo->save();
+      }
+      //$pre_url = url()->previous();
+      return redirect("/shops/".$request->shop_id);
   }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,15 +7,40 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Auth;
 use App\User;
+use App\Review;
 
 class UsersController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
   public function show($id)
   {
-      $user = User::find($id);
       $name = Auth::user()->name;
       // ユーザのカレーのレビューやお気に入りを送る (今後実装)
+      $shop_reviews = Review::whereNotIn('shop_id', [0])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
+      $curry_reviews = Review::whereNotIn('curry_id', [0])->where('user_id', $id)->orderBy('created_at', 'desc')->get();
+      //$curry_reviews = User::find($id)->reviews()->orderBy('created_at', 'desc')->get();
       //$curries = User::find($id)->curries()->orderBy('created_at', 'DESC')->paginate(5);
-      return view('users.show')->with(['user' =>  $user, 'name' => $name]);//(array('name' => $name, 'curries' => $curries));
+      return view('users.show')->with(array('curry_reviews' => $curry_reviews, 'shop_reviews' => $shop_reviews));
+  }
+
+  public function bookmarks()
+  {
+      $id = Auth::user()->id;
+      $bookmarks = Auth::user()->bookmarks()->where('user_id', $id)->paginate(12);
+      return view('users.bookmarks')->with('bookmarks', $bookmarks);
+  }
+
+  public function likes()
+  {
+      return view('users.likes');
+  }
+
+  public function profile()
+  {
+      return view('users.profile');
   }
 }
