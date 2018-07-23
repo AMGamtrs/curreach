c8abdcdc584b934e39906ce2ebb32abfbdc2f34e
tech-camp <>
2018-07-23 09:53:53 +0900

    edit views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,22 +4,22 @@
 
       <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8 col-sm-10 col-md-offset-2 col-sm-offset-1">
                 <div class="panel panel-warning" style="padding:0;">
-                    <div class="panel-heading">ログイン</div>
+                    <div class="panel-heading"><strong><span class="glyphicon glyphicon-user" aria-hidden="true"></span> ログイン</strong></div>
                     <div class="panel-body">
 
     {{ Form::open(['class' => 'form-horizontal']) }}
         <div class="field form-group">
-            <label class="col-md-4 control-label">メールアドレス</label>
-            <div class="col-md-6">
+            <label class="col-md-4 col-sm-4 control-label">メールアドレス</label>
+            <div class="col-md-6 col-sm-6">
               <input class="form-control" type="email" name="email" autofocus="autofocus">
             </div>
         </div>
 
         <div class="field form-group">
-            <label class="col-md-4 control-label">パスワード</label>
-            <div class="col-md-6">
+            <label class="col-md-4 col-sm-4 control-label">パスワード</label>
+            <div class="col-md-6 col-sm-6">
               <input class="form-control" type="password" name="password" autocomplete="off">
             </div>
         </div>
